e022d54b167c5d467a9fe058ba56c6572268a1e9
KhanVl <vovahan2002@gmail.com>
2024-11-21 08:19:20 +0900

    page-1 and page-2 reverse

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # the-first-exercise
+
+index.html - 5.8
+page-1.html - 6.8
+page-2.html - 7.5
--- js/app.js
+++ js/app.js
@@ -13,4 +13,40 @@ function readMore() {
         more.style.display="inline";
     }
     
+};
+
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const block = document.getElementById('myBlock');
+    if (block.style.display === 'none' || block.style.display === '') {
+        block.style.display = 'flex';
+        this.textContent = 'Скрыть блок';
+    } else {
+        block.style.display = 'none';
+        this.textContent = 'Показать блок';
+    }
+});
+
+
+
+
+function initSwiper() {
+    if (window.innerWidth < 768) {
+        swiper = new Swiper('.swiper', {
+            direction: 'horizontal',
+            slidesPerView: 1.5,
+            spaceBetween: 10,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    } else if (swiper) {
+        swiper.destroy(); // Уничтожаем Swiper, если он уже инициализирован
+        swiper = null; // Обнуляем переменную
+    }
 }
+
+window.addEventListener('resize', initSwiper);
+window.addEventListener('load', initSwiper);
